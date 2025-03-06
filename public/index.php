106b9d150f55c1f9d0cbe75b9ba312cae09cfa5e
Gabriel Ferreira <ferreirawow@gmail.com>
2025-03-06 23:18:38 +0000

    File System Initial Commit

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+// Load Composer Dependencies
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// Load env variables
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+// Enable logging system
+$log = new Logger('app');
+$log->pushHandler(new StreamHandler(__DIR__ . '/../logs/app.log', Logger::DEBUG));
+$log->info('Application has started');
+
+// Routing
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+switch ($uri) {
+  case '':
+    break;
+}
+?>
